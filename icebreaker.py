049880d997d3327bf009d5832085c66b6efad134
nikhilku8281 <nikhil.ku.8281@gmail.com>
2023-12-20 23:42:08 -0500

    My first Langchain development

---
+++ icebreaker.py
@@ -0,0 +1,37 @@
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
+from third_parties.linkedin import scrape_linkedin_profile
+from third_parties.linkedin import scrape_gisthub_profile
+from agents.linkedin_lookup_agent import lookup
+
+information = """
+    Narendra Modi
+"""
+
+if __name__ == "__main__":
+    print("Hello Langchain ")
+
+    linkedin_profile_url = lookup("Nikhil Kumar Senior Product manager Thomson reuters Canada")
+
+    summary_template = """
+        given the LinkedIn information {information} about a person from what I want you to create:
+        1. a short summary
+        2. two interesting facts about them
+    """
+    summary_promt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    chain = LLMChain(llm=llm, prompt=summary_promt_template)
+    ## Below code to be used at time of using Linkedin API
+    # linkedin_data=scrape_linkedin_profile(linkedin_profile_url='https://www.linkedin.com/in/harrison-chase-961287118')
+
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
+
+    print(chain.run(information=linkedin_data))
+    # gisthub = scrape_gisthub_profile(
+    #     "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json"
+    # )
+    # print(chain.run(information=gisthub))
---
+++ third_parties/linkedin.py
@@ -0,0 +1,44 @@
+import os
+import requests
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str):
+    """Scrape information from LinkedIn profiles,
+    Manually crape the information from the LinkedIn profile
+    :param linkedin_profile_url:
+    :return:
+    """
+    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+    header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+
+    response = requests.get(
+        api_endpoint, params={"url": linkedin_profile_url}, headers=header_dic
+    )
+    data = response.json()
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+    return data
+
+
+def scrape_gisthub_profile(linkedin_profile_url: str):
+    linkedin_data = requests.get(linkedin_profile_url)
+    # print(linkedin_data.json())
+    data = linkedin_data.json()
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+    return data
